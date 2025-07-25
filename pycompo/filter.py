33e0b1eb6eb67e7b976a3bd06a98b4ce19aea1d5
Henning Franke <henning.franke@mpimet.mpg.de>
2025-07-25 08:35:38 +0200

    Initial working version of anomaly extraction based on a Gaussian filter.

---
+++ pycompo/filter.py
@@ -0,0 +1,136 @@
+import numpy as np
+import xarray as xr
+from scipy.ndimage import gaussian_filter
+from typing import Tuple
+
+
+def get_gaussian_filter_bg_ano(
+        dset: xr.Dataset | xr.DataArray,
+        **kwargs,
+        ) -> xr.Dataset:
+    """
+    Applies a Gaussian lowpass filter to each variable in the input dataset and
+    computes the background and anomaly fields.
+
+    Parameters
+    ----------
+    dset : xr.Dataset or xr.DataArray
+        Input dataset or data array containing the variables to be filtered.
+    **kwargs
+        Additional keyword arguments passed to `gaussian_lowpass_filter`.
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset with added variables for each input variable:
+        - '<var>_bg': the background field after Gaussian filtering.
+        - '<var>_ano': the anomaly field (original minus background).
+
+    Notes
+    -----
+    If a DataArray is provided, it is converted to a Dataset. The anomaly is
+    computed as the difference between the original variable and its background
+    field.
+    """
+    if isinstance(dset, xr.DataArray):
+        dset = dset.to_dataset()
+
+    for var in dset.data_vars:
+        dset[f'{var}_bg'] = (gaussian_lowpass_filter(dset[var], **kwargs))
+        dset[f'{var}_ano'] = dset[var] - dset['ts_bg']
+
+    return dset
+
+
+def gaussian_lowpass_filter(
+        dset: xr.DataArray,
+        lat_mid: float,
+        Lc_km: float,
+        truncate: float, 
+        ) -> Tuple[list, np.ndarray]:
+    """
+    Applies a Gaussian lowpass filter to the input DataArray.
+
+    Parameters
+    ----------
+    dset : xr.DataArray
+        Input data array containing the variable to be filtered. Must have 'lat' and 'lon' dimensions.
+    lat_mid : float
+        Latitude at which to compute the conversion from kilometers to degrees.
+    Lc_km : float
+        Cutoff length scale in kilometers for the Gaussian filter.
+    truncate : float
+        Truncate value for the Gaussian filter.
+
+    Returns
+    -------
+    list
+        List of dimension names of the input DataArray, in the order used for filtering.
+    np.ndarray
+        Filtered data as a NumPy array after applying the Gaussian filter.
+
+    Raises
+    ------
+    ValueError
+        If the input DataArray does not contain 'lat' and 'lon' dimensions.
+    """
+    # Some checks and preprocessing of the dataset
+    if not all([dim in dset.dims for dim in ['lat', 'lon']]):
+        raise ValueError(
+            "Please provide a dataset that contains 'lat' and 'lon' as " +
+            "dimension names for latitude and longitude, respectively."
+            )
+    dset = dset.transpose('lat', 'lon', ...)
+    n_dims = len(dset.dims)
+
+    # Calculate degrees per gridpoint
+    dlat = dset['lat'].diff('lat').mean().values
+    dlon = dset['lon'].diff('lon').mean().values
+
+    # Define cutoff wavenumber (in radians per degree)
+    Lc_deg_lat, Lc_deg_lon = _Lc_km2deg(Lc_km, lat_mid)  # degrees
+    kc_deg_lat = 2 * np.pi / Lc_deg_lat  # radians per degree
+    kc_deg_lon = 2 * np.pi / Lc_deg_lon  # radians per degree
+
+    # Compute sigma in grid points
+    sigma_x = (2 * np.pi) / (kc_deg_lon * np.sqrt(2) * dlon)
+    sigma_y = (2 * np.pi) / (kc_deg_lat * np.sqrt(2) * dlat)
+    sigmas = [sigma_y, sigma_x]
+    while len(sigmas) < n_dims: sigmas.append(0)
+
+    return list(dset.dims), gaussian_filter(
+        dset, sigma=sigmas, truncate=truncate
+        )
+
+
+def _Lc_km2deg(
+        Lc_km: float,
+        lat_mid: float
+        ) -> Tuple[float, float]:
+    """
+    Convert a length in kilometers to degrees of latitude and longitude at a
+    given latitude.
+
+    Parameters
+    ----------
+    Lc_km : float
+        Length in kilometers to be converted.
+    lat_mid : float
+        Latitude (in degrees) at which the conversion is performed.
+
+    Returns
+    -------
+    Tuple[float, float]
+        A tuple containing:
+        - The equivalent length in degrees of latitude.
+        - The equivalent length in degrees of longitude at the specified
+        latitude.
+
+    Notes
+    -----
+    - The conversion uses the average value of 1 degree latitude ≈ 111.195 km.
+    - The longitude conversion accounts for the cosine of the latitude.
+    """
+    deg_per_km_lat = 1/111.195
+    deg_per_km_lon = 1/(111.195 * np.cos(np.deg2rad(lat_mid)))
+    return (Lc_km * deg_per_km_lat, Lc_km * deg_per_km_lon)
