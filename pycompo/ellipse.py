a6cbb2143044fc7127a95f3407deba63a111e41d
Henning Franke <henning.franke@mpimet.mpg.de>
2025-08-20 14:57:21 +0200

    Plotting of ellipses in various coordinate systems

--- pycompo/ellipse.py
+++ pycompo/ellipse.py
@@ -1,9 +1,33 @@
 import numpy as np
+import xarray as xr
 from typing import Tuple
+
+import pycompo.coord as pccoord
+
 
 def calc_polar_angle_rad(
         x: float
         ) -> float:
+    """
+    Calculate polar angle in radians from ellipse orientation
+
+    This function converts an orientation angle `x` (in radians), measured
+    counter-clockwise from the x-axis, to the corresponding polar angle,
+    measured clockwise from the x-axis, for the major axis of an ellipse.
+    The conversion is performed as follows:
+    - If `x` is positive, the polar angle is computed as (π/2 - x).
+    - If `x` is zero or negative, the polar angle is computed as (-π/2 - x).
+
+    Parameters
+    ----------
+    x : float
+        The orientation angle of the ellipse in radians.
+
+    Returns
+    -------
+    float
+        The corresponding polar angle in radians.
+    """
     return (np.pi/2 - x) if x > 0 else (-np.pi/2 - x)
 
 
@@ -11,6 +35,25 @@ def calc_axis_major_end(
         axis_major_length: float,
         polar_angle_rad: float,
         ) -> Tuple[float, float]:
+    """
+    Calculate the end point coordinates of the major axis of an ellipse.
+
+    Given the length of the major axis and the polar angle (in radians),
+    this function computes the (x, y) coordinates of the end point of the
+    major axis, assuming the ellipse is centered at the origin.
+
+    Parameters
+    ----------
+    axis_major_length : float
+        The total length of the major axis of the ellipse.
+    polar_angle_rad : float
+        The polar angle (in radians) from the x-axis to the major axis.
+
+    Returns
+    -------
+    Tuple[float, float]
+        The (x, y) coordinates of the end point of the major axis.
+    """
     x_end = axis_major_length / 2 * np.cos(polar_angle_rad)
     y_end = axis_major_length / 2 * np.sin(polar_angle_rad)
     return (x_end, y_end)
@@ -20,6 +63,77 @@ def calc_axis_minor_end(
         axis_minor_length: float,
         polar_angle_rad: float,
         ) -> Tuple[float, float]:
+    """
+    Calculate the end point coordinates of the minor axis of an ellipse.
+
+    Given the length of the minor axis and the polar angle (in radians),
+    this function computes the (x, y) coordinates of the end point of the
+    minor axis, assuming the ellipse is centered at the origin.
+
+    Parameters
+    ----------
+    axis_minor_length : float
+        The length of the minor axis of the ellipse.
+    polar_angle_rad : float
+        The orientation angle of the ellipse in radians (measured from the x-axis).
+
+    Returns
+    -------
+    Tuple[float, float]
+        The (x, y) coordinates of the end point of the minor axis.
+    """
     x_end = axis_minor_length / 2 * np.cos(polar_angle_rad + np.pi / 2)
     y_end = axis_minor_length / 2 * np.sin(polar_angle_rad + np.pi / 2)
     return (x_end, y_end)
+
+
+def get_ellipse_featcen_spherical_coords(
+        props: xr.Dataset,
+        basic_dcoords: Tuple[float, float],
+    ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    dlat = basic_dcoords[0]
+    dlon = basic_dcoords[1]
+    major_end_idx = props['axis_major_end_idx'].values
+    minor_end_idx = props['axis_minor_end_idx'].values
+
+    major_end_spherical = (major_end_idx[0] * dlon, major_end_idx[1] * dlat)
+    minor_end_spherical = (minor_end_idx[0] * dlon, minor_end_idx[1] * dlat)
+    return (major_end_spherical, minor_end_spherical)
+
+
+def get_ellipse_featcen_cartesian_coords(
+        props: xr.Dataset,
+        basic_coords: Tuple[xr.DataArray, xr.DataArray],
+        basic_dcoords: Tuple[float, float],
+    ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    dlat = basic_dcoords[0]
+    dlon = basic_dcoords[1]
+    major_end_idx = props['axis_major_end_idx'].values
+    minor_end_idx = props['axis_minor_end_idx'].values
+
+    centroid_lat, _ = pccoord._get_centroid_coords(
+        basic_coords, basic_dcoords, props['centroid_idx'],
+        )
+    dx = dlon * 111.195 * np.cos(np.deg2rad(centroid_lat + dlat))
+    dy = dlat * 111.195
+
+    major_end_cartesian = (major_end_idx[0] * dx, major_end_idx[1] * dy)
+    minor_end_cartesian = (minor_end_idx[0] * dx, minor_end_idx[1] * dy)
+    return (major_end_cartesian, minor_end_cartesian)
+
+
+def get_ellipse_featcen_rotated_cartesian_coords(
+        props: xr.Dataset,
+        basic_coords: Tuple[xr.DataArray, xr.DataArray],
+        basic_dcoords: Tuple[float, float],
+    ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    major_end_cartesian, minor_end_cartesian = \
+        get_ellipse_featcen_cartesian_coords(props, basic_coords, basic_dcoords)
+    
+    major_end_rotated_cartesian = pccoord._calc_rotated_feature_centric_coords(
+        major_end_cartesian[0], major_end_cartesian[1], props['polar_angle_rad']
+        )
+    minor_end_rotated_cartesian = pccoord._calc_rotated_feature_centric_coords(
+        minor_end_cartesian[0], minor_end_cartesian[1], props['polar_angle_rad']
+        )
+    return (major_end_rotated_cartesian, minor_end_rotated_cartesian)
---
+++ pycompo/plot.py
@@ -0,0 +1,250 @@
+import numpy as np
+import xarray as xr
+from typing import Tuple
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from matplotlib.axes import Axes
+
+import pycompo.coord as pccoord
+import pycompo.ellipse as pcellipse
+
+def plot_preprocessing_overview_map(
+        dset: xr.Dataset,
+        var: str,
+        dT_thresh: float,
+        ano_range: Tuple[float, float],
+        ano_cmap: str,
+        ) -> None:
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    # filled contour plots
+    dset['ts'].plot(ax=axs[0, 0]) # type: ignore
+    axs[0, 0].set_title('Original SST')
+
+    dset[f'{var}_detrend'].plot(
+        ax=axs[0, 1], vmin=ano_range[0], vmax=ano_range[1], cmap=ano_cmap,
+        extend='both',
+        ) # type: ignore
+    axs[0, 1].set_title('Detrended SST')
+
+    dset[f'{var}_detrend_bg'].plot(
+        ax=axs[1, 0], vmin=ano_range[0], vmax=ano_range[1], cmap=ano_cmap,
+        extend='both',
+        ) # type: ignore
+    axs[1, 0].set_title('Detrended SST background (Lowpass)')
+
+    dset[f'{var}_detrend_ano'].plot(
+        ax=axs[1, 1], vmin=ano_range[0], vmax=ano_range[1], cmap=ano_cmap,
+        extend='both',
+        ) # type: ignore
+    axs[1, 1].set_title('Detrended SST anomaly')
+
+    # overlay contour lines for anomalies
+    for i in range(0, 4):
+        axs.ravel()[i].contour(
+            dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
+            levels=[-dT_thresh, dT_thresh], colors='k',
+            )
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_coord_transformation(
+        dset: xr.Dataset,
+        feature_props: xr.Dataset,
+        basic_coords: Tuple[xr.DataArray, xr.DataArray],
+        var: str,
+        dT_thresh: float
+        ) -> None:
+    # Get geophyiscal coordinate of feature centroid
+    basic_dcoords = (
+        basic_coords[0].diff('lat').mean().values,
+        basic_coords[1].diff('lon').mean().values,
+    )
+    centroid = pccoord._get_centroid_coords(
+        basic_coords, basic_dcoords, feature_props['centroid_idx']
+        )
+    
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    # Plot data in regular geophysical coordinates
+    pl0 = axs[0, 0].pcolormesh(
+        dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
+        cmap='RdBu_r', shading='auto'
+        )
+    axs[0, 0].contour(
+        dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
+        levels=[-dT_thresh, dT_thresh], colors='k',
+        )
+    axs[0, 0].set_title('Regular geophysical coordinate')
+    axs[0, 0].scatter(centroid[1], centroid[0], color='k')
+    axs[0, 0].set_xlabel('Longitude / $^{\circ}\,$E')
+    axs[0, 0].set_ylabel('Latitude / $^{\circ}\,$N')
+    plt.colorbar(
+        pl0, ax=axs[0, 0], orientation='vertical', extend='both',
+        label=f'{var}_detrend_ano / K',
+        )
+
+    # Plot data in feature-centric geophysical coordinates
+    pl1 = axs[0, 1].pcolormesh(
+        dset['feature_centric_lon'], dset['feature_centric_lat'],
+        dset[f'{var}_detrend_ano'],
+        cmap='RdBu_r', shading='auto'
+        )
+    axs[0, 1].contour(
+        dset['feature_centric_lon'], dset['feature_centric_lat'],
+        dset[f'{var}_detrend_ano'],
+        levels=[-dT_thresh, dT_thresh], colors='k',
+        )
+    axs[0, 1].scatter(0, 0, color='k')
+
+    axs[0, 1].set_title('Feature-centric geophysical coordinate')
+    axs[0, 1].set_xlabel('Feature-centric longitude / $^{\circ}\,$E')
+    axs[0, 1].set_ylabel('Feature-centric latitude / $^{\circ}\,$N')
+    plt.colorbar(
+        pl1, ax=axs[0, 1], orientation='vertical', extend='both',
+        label=f'{var}_detrend_ano / K',
+        )
+    
+    # Plot data in feature-centric Cartesian coordinates
+    pl1 = axs[1, 0].pcolormesh(
+        dset['feature_centric_x'], dset['feature_centric_y'],
+        dset[f'{var}_detrend_ano'],
+        cmap='RdBu_r', shading='auto'
+        )
+    axs[1, 0].contour(
+        dset['feature_centric_x'], dset['feature_centric_y'],
+        dset[f'{var}_detrend_ano'],
+        levels=[-dT_thresh, dT_thresh], colors='k',
+        )
+    axs[1, 0].scatter(0, 0, color='k')
+
+    axs[1, 0].set_title('Feature-centric Cartesian coordinate')
+    axs[1, 0].set_xlabel('Feature-centric distance / km')
+    axs[1, 0].set_ylabel('Feature-centric distance / km')
+    plt.colorbar(
+        pl1, ax=axs[1, 0], orientation='vertical', extend='both',
+        label=f'{var}_detrend_ano / K',
+        )
+    
+
+    # Plot data in rotaed feature-centric Cartesian coordinates
+    pl1 = axs[1, 1].pcolormesh(
+        dset['rotated_feature_centric_x'], dset['rotated_feature_centric_y'],
+        dset[f'{var}_detrend_ano'],
+        cmap='RdBu_r', shading='auto'
+        )
+    axs[1, 1].contour(
+        dset['rotated_feature_centric_x'], dset['rotated_feature_centric_y'],
+        dset[f'{var}_detrend_ano'],
+        levels=[-dT_thresh, dT_thresh], colors='k',
+        )
+    axs[1, 1].scatter(0, 0, color='k')
+
+    axs[1, 1].set_title('Roated feature-centric Cart. coord.')
+    axs[1, 1].set_xlabel('Feature-centric distance / km')
+    axs[1, 1].set_ylabel('Feature-centric distance / km')
+    plt.colorbar(
+        pl1, ax=axs[1, 1], orientation='vertical', extend='both',
+        label=f'{var}_detrend_ano / K',
+        )
+
+    # Plot grid lines
+    axs[0, 1].vlines(
+        x=0, ymin=dset['feature_centric_lat'].min(),
+        ymax=dset['feature_centric_lat'].max(), lw=0.8, ls='--', color='gray'
+        )
+    axs[0, 1].hlines(
+        y=0, xmin=dset['feature_centric_lon'].min(),
+        xmax=dset['feature_centric_lon'].max(), lw=0.8, ls='--', color='gray'
+        )
+    # Plot grid lines
+    axs[1, 0].vlines(
+        x=0, ymin=dset['feature_centric_y'].min(),
+        ymax=dset['feature_centric_y'].max(), lw=0.8, ls='--', color='gray'
+        )
+    axs[1, 0].hlines(
+        y=0, xmin=dset['feature_centric_x'].min(),
+        xmax=dset['feature_centric_x'].max(), lw=0.8, ls='--', color='gray'
+        )
+    
+    # Plot grid lines
+    axs[1, 1].vlines(
+        x=0, ymin=dset['rotated_feature_centric_y'].min(),
+        ymax=dset['rotated_feature_centric_y'].max(), lw=0.8, ls='--',
+        color='gray',
+        )
+    axs[1, 1].hlines(
+        y=0, xmin=dset['rotated_feature_centric_x'].min(),
+        xmax=dset['rotated_feature_centric_x'].max(), lw=0.8, ls='--',
+        color='gray',
+        )
+
+    # Plot ellipse features
+    _plot_feature_ellipse_spherical(axs[0, 1], feature_props, basic_dcoords)
+    _plot_feature_ellipse_cartesian(
+        axs[1, 0], feature_props, basic_coords, basic_dcoords
+        )
+    _plot_feature_ellipse_rotated_cartesian(
+        axs[1, 1], feature_props, basic_coords, basic_dcoords
+        )
+
+    for i in range(0, len(axs.ravel())): axs.ravel()[i].set_aspect('equal')
+    plt.tight_layout()
+    plt.show()
+    
+    
+def _plot_feature_ellipse_spherical(
+        axis: Axes,
+        props: xr.Dataset,
+        basic_dcoords: Tuple[float, float]):
+    polar_angle_rad = props['polar_angle_rad'].values
+    major_end, minor_end = pcellipse.get_ellipse_featcen_spherical_coords(
+        props, basic_dcoords,
+        )
+    _plot_feature_ellipse(axis, polar_angle_rad, major_end, minor_end)
+
+
+def _plot_feature_ellipse_cartesian(
+        axis: Axes,
+        props: xr.Dataset,
+        basic_coords: Tuple[xr.DataArray, xr.DataArray],
+        basic_dcoords: Tuple[float, float],
+        ):
+    polar_angle_rad = props['polar_angle_rad'].values
+    major_end, minor_end = pcellipse.get_ellipse_featcen_cartesian_coords(
+        props, basic_coords, basic_dcoords,
+        )
+    _plot_feature_ellipse(axis, polar_angle_rad, major_end, minor_end)
+
+
+def _plot_feature_ellipse_rotated_cartesian(
+        axis: Axes,
+        props: xr.Dataset,
+        basic_coords: Tuple[xr.DataArray, xr.DataArray],
+        basic_dcoords: Tuple[float, float],
+        ):
+    major_end, minor_end = \
+        pcellipse.get_ellipse_featcen_rotated_cartesian_coords(
+            props, basic_coords, basic_dcoords,
+            )
+    _plot_feature_ellipse(axis, 0.0, major_end, minor_end)
+
+
+def _plot_feature_ellipse(
+        axis: Axes,
+        polar_angle_rad: float,
+        major_end: Tuple[float, float],
+        minor_end: Tuple[float, float]):
+    ellipse = Ellipse(
+        xy=(0, 0),
+        width=np.sqrt((2 * major_end[0]) ** 2 + (2 * major_end[1]) ** 2),
+        height=np.sqrt((2 * minor_end[0]) ** 2 + (2 * minor_end[1]) ** 2),
+        angle=np.rad2deg(polar_angle_rad),
+        edgecolor='green', facecolor='none', lw=1, ls='-.'
+    )
+    axis.add_patch(ellipse)
+    
+    axis.plot([-major_end[0], major_end[0]], [-major_end[1], major_end[1]])
+    axis.plot([-minor_end[0], minor_end[0]], [-minor_end[1], minor_end[1]])
