d85a105500f725cd172ac0b0d90470ec56deef71
Henning Franke <henning.franke@mpimet.mpg.de>
2025-08-29 10:16:31 +0200

    Make plotting cleaner and nicer

--- pycompo/plot.py
+++ pycompo/plot.py
@@ -1,8 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import pycompo.ellipse
 import xarray as xr
 from typing import Tuple
-import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from matplotlib.axes import Axes
 
@@ -43,7 +42,7 @@ def plot_preprocessing_overview_map(
     for i in range(0, 4):
         axs.ravel()[i].contour(
             dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
-            levels=[-dT_thresh, dT_thresh], colors='k',
+            levels=[dT_thresh], colors='k',
             )
 
     plt.tight_layout()
@@ -58,154 +57,132 @@ def plot_coord_trafo(
         var: str,
         dT_thresh: float
         ) -> None:
-    _, axs = plt.subplots(3, 2, figsize=(10, 12))
-
     dset = featcen_dset[feature_id]
     winds = winds.isel(feature=feature_id)
+    _, axs = plt.subplots(3, 2, figsize=(10, 12))
+
+    clabel = f'{var} / K'
 
     # Plot data in regular geophysical coordinates
     pl0 = axs[0, 0].pcolormesh(
-        dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto'
+        dset['lon'], dset['lat'], dset[var], cmap='RdBu_r',
         )
     axs[0, 0].contour(
-        dset['lon'], dset['lat'], dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['lon'], dset['lat'], dset[var], levels=[dT_thresh], colors='gray',
         )
     axs[0, 0].scatter(
         ellipse['sphere']['centroid'].\
             sel(component='lon').isel(feature=feature_id),
         ellipse['sphere']['centroid'].\
             sel(component='lat').isel(feature=feature_id),
-        color='k'
+        color='gray',
         )
-    
     axs[0, 0].set_title('Regular geophysical coordinate')
     axs[0, 0].set_xlabel('Longitude / $^{\circ}\,$E')
     axs[0, 0].set_ylabel('Latitude / $^{\circ}\,$N')
     plt.colorbar(
-        pl0, ax=axs[0, 0], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl0, ax=axs[0, 0], orientation='vertical', extend='both', label=clabel,
         )
 
     # Plot data in feature-centric geophysical coordinates
     pl1 = axs[0, 1].pcolormesh(
-        dset['featcen_lon'], dset['featcen_lat'], dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto',
+        dset['featcen_lon'], dset['featcen_lat'], dset[var], cmap='RdBu_r',
         )
     axs[0, 1].contour(
-        dset['featcen_lon'], dset['featcen_lat'], dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['featcen_lon'], dset['featcen_lat'], dset[var],
+        levels=[dT_thresh], colors='gray',
         )
     axs[0, 1].scatter(
         ellipse['featcen_sphere']['centroid'].\
             sel(component='lon').isel(feature=feature_id),
         ellipse['featcen_sphere']['centroid'].\
             sel(component='lat').isel(feature=feature_id),
-        color='k'
+        color='gray',
         )
-
     axs[0, 1].set_title('Feature-centric geophysical coordinate')
     axs[0, 1].set_xlabel('Feature-centric longitude / $^{\circ}\,$E')
     axs[0, 1].set_ylabel('Feature-centric latitude / $^{\circ}\,$N')
     plt.colorbar(
-        pl1, ax=axs[0, 1], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl1, ax=axs[0, 1], orientation='vertical', extend='both', label=clabel,
         )
     
     # Plot data in feature-centric Cartesian coordinates
     pl1 = axs[1, 0].pcolormesh(
-        dset['featcen_x'], dset['featcen_y'], dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto'
+        dset['featcen_x'], dset['featcen_y'], dset[var], cmap='RdBu_r',
         )
     axs[1, 0].contour(
-        dset['featcen_x'], dset['featcen_y'], dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['featcen_x'], dset['featcen_y'], dset[var],
+        levels=[dT_thresh], colors='gray',
         )
     axs[1, 0].scatter(
         ellipse['featcen_cart']['centroid'].\
             sel(component='x').isel(feature=feature_id),
         ellipse['featcen_cart']['centroid'].\
             sel(component='y').isel(feature=feature_id),
-        color='k'
+        color='gray',
         )
-
     axs[1, 0].set_title('Feature-centric Cartesian coordinate')
     axs[1, 0].set_xlabel('Feature-centric distance / km')
     axs[1, 0].set_ylabel('Feature-centric distance / km')
     plt.colorbar(
-        pl1, ax=axs[1, 0], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl1, ax=axs[1, 0], orientation='vertical', extend='both', label=clabel,
         )
     
     # Plot data in rotated feature-centric Cartesian coordinates
     pl1 = axs[1, 1].pcolormesh(
-        dset['rota_featcen_x'], dset['rota_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto'
+        dset['rota_featcen_x'], dset['rota_featcen_y'], dset[var],
+        cmap='RdBu_r',
         )
     axs[1, 1].contour(
-        dset['rota_featcen_x'], dset['rota_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['rota_featcen_x'], dset['rota_featcen_y'], dset[var],
+        levels=[dT_thresh], colors='gray',
         )
     axs[1, 1].scatter(
         ellipse['rota_featcen_cart']['centroid'].\
             sel(component='x').isel(feature=feature_id),
         ellipse['rota_featcen_cart']['centroid'].\
             sel(component='y').isel(feature=feature_id),
-        color='k'
+        color='gray',
         )
-
     axs[1, 1].set_title('Rotated feature-centric Cartesian coordinate')
     axs[1, 1].set_xlabel('Feature-centric distance / km')
     axs[1, 1].set_ylabel('Feature-centric distance / km')
     plt.colorbar(
-        pl1, ax=axs[1, 1], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl1, ax=axs[1, 1], orientation='vertical', extend='both', label=clabel,
         )
     
     # Plot data in normalized rotated feature-centric Cartesian coordinates
     pl1 = axs[2, 0].pcolormesh(
-        dset['En_rota_featcen_x'], dset['En_rota_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto'
+        dset['En_rota_featcen_x'], dset['En_rota_featcen_y'], dset[var],
+        cmap='RdBu_r',
         )
     axs[2, 0].contour(
-        dset['En_rota_featcen_x'], dset['En_rota_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['En_rota_featcen_x'], dset['En_rota_featcen_y'], dset[var],
+        levels=[dT_thresh], colors='gray',
         )
-    axs[2, 0].scatter([0], [0], color='k')
-
+    axs[2, 0].scatter([0], [0], color='gray')
     axs[2, 0].set_title('Norm. rotated feature-centric Cartesian coordinate')
     axs[2, 0].set_xlabel('Fractional distance')
     axs[2, 0].set_ylabel('Fractional distance')
     plt.colorbar(
-        pl1, ax=axs[2, 0], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl1, ax=axs[2, 0], orientation='vertical', extend='both', label=clabel,
         )
-    
 
     # Plot data in normalized rotated feature-centric Cartesian coordinates
     pl1 = axs[2, 1].pcolormesh(
-        dset['En_rota2_featcen_x'], dset['En_rota2_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        cmap='RdBu_r', shading='auto'
+        dset['En_rota2_featcen_x'], dset['En_rota2_featcen_y'], dset[var],
+        cmap='RdBu_r',
         )
     axs[2, 1].contour(
-        dset['En_rota2_featcen_x'], dset['En_rota2_featcen_y'],
-        dset[f'{var}_detrend_ano'],
-        levels=[-dT_thresh, dT_thresh], colors='k',
+        dset['En_rota2_featcen_x'], dset['En_rota2_featcen_y'], dset[var],
+        levels=[dT_thresh], colors='gray',
         )
-    axs[2, 1].scatter([0], [0], color='k')
-
+    axs[2, 1].scatter([0], [0], color='gray')
     axs[2, 1].set_title('Norm. rotated feature-centric Cartesian coordinate')
     axs[2, 1].set_xlabel('Downwind fractional distance')
     axs[2, 1].set_ylabel('Downwind fractional distance')
     plt.colorbar(
-        pl1, ax=axs[2, 1], orientation='vertical', extend='both',
-        label=f'{var}_detrend_ano / K',
+        pl1, ax=axs[2, 1], orientation='vertical', extend='both', label=clabel,
         )
 
     # Plot grid lines
@@ -254,32 +231,38 @@ def plot_coord_trafo(
     _plot_feature_ellipse(
         axs[1, 1], ellipse['rota_featcen_cart'].isel(feature=feature_id)
         )
-    circle1 = plt.Circle([0, 0], 1, fill=False, color='green', ls='-.')
-    circle2 = plt.Circle([0, 0], 1, fill=False, color='green', ls='-.')
+    circle1 = plt.Circle(
+        [0, 0], 1, fill=False, color='k', ls='-.', lw=1.5, zorder=2,
+        )
+    circle2 = plt.Circle(
+        [0, 0], 1, fill=False, color='k', ls='-.', lw=1.5, zorder=2,
+    )
     axs[2, 0].add_patch(circle1)
     axs[2, 1].add_patch(circle2)
     
     # Plot wind features
     axs[0, 0].quiver(
-        ellipse['sphere']['centroid'].sel(component='lon').isel(feature=feature_id),
-        ellipse['sphere']['centroid'].sel(component='lat').isel(feature=feature_id),
-        winds['bg_uas'], winds['bg_vas'], scale=50,
+        ellipse['sphere']['centroid'].sel(component='lon').\
+            isel(feature=feature_id),
+        ellipse['sphere']['centroid'].sel(component='lat').\
+            isel(feature=feature_id),
+        winds['bg_uas'], winds['bg_vas'], scale=50, zorder=2,
         )
-    axs[0, 1].quiver(0, 0, winds['bg_uas'], winds['bg_vas'], scale=50)
-    axs[1, 0].quiver(0, 0, winds['bg_uas'], winds['bg_vas'], scale=50)
+    axs[0, 1].quiver(0, 0, winds['bg_uas'], winds['bg_vas'], scale=50, zorder=2)
+    axs[1, 0].quiver(0, 0, winds['bg_uas'], winds['bg_vas'], scale=50, zorder=2)
     
     uas_rota, vas_rota = pccoord._calc_rota_featcen_cart_coords(
         winds['bg_uas'], winds['bg_vas'],
         ellipse['featcen_cart']['polar_angle_rad'].isel(feature=feature_id),
         )
-    axs[1, 1].quiver(np.array([0]), np.array([0]), uas_rota, vas_rota, scale=50)
-    axs[2, 0].quiver(np.array([0]), np.array([0]), uas_rota, vas_rota, scale=50)
+    axs[1, 1].quiver(0, 0, uas_rota, vas_rota, scale=50, zorder=2)
+    axs[2, 0].quiver(0, 0, uas_rota, vas_rota, scale=50, zorder=2)
     
     uas_rota2, vas_rota2 = pccoord._calc_rota_featcen_cart_coords(
         winds['bg_uas'], winds['bg_vas'],
         np.arctan2(winds['bg_vas'], winds['bg_uas']),
         )
-    axs[2, 1].quiver(np.array([0]), np.array([0]), uas_rota2, vas_rota2, scale=50)
+    axs[2, 1].quiver(0, 0, uas_rota2, vas_rota2, scale=50, zorder=2)
 
     axs[2, 0].set_xlim([-2.5, 2.5])
     axs[2, 0].set_ylim([-2.5, 2.5])
@@ -304,15 +287,17 @@ def _plot_feature_ellipse(
         width=np.sqrt((2 * maj_end[0]) ** 2 + (2 * maj_end[1]) ** 2),
         height=np.sqrt((2 * min_end[0]) ** 2 + (2 * min_end[1]) ** 2),
         angle=np.rad2deg(polar_angle_rad),
-        edgecolor='green', facecolor='none', lw=1, ls='-.',
+        edgecolor='k', facecolor='none', lw=1.5, ls='-.', zorder=2,
     )
     axis.add_patch(ellipse_patch)
     
     axis.plot(
         [-maj_end.isel(component=1), maj_end.isel(component=1)],
         [-maj_end.isel(component=0), maj_end.isel(component=0)],
+        zorder=2,
         )
     axis.plot(
         [-min_end.isel(component=1), min_end.isel(component=1)],
         [-min_end.isel(component=0), min_end.isel(component=0)],
+        zorder=2,
         )
