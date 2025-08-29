d629d39966856bbb35fe1cd8c2dd96f442cebe0a
Henning Franke <henning.franke@mpimet.mpg.de>
2025-08-29 17:19:05 +0200

    First shell script potentially able to run operationally

---
+++ run/run_pycompo.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+CONFIG_FILE=./../config/settings_template.yaml
+
+# GET FEATURES
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=mh0731
+#SBATCH --partition=compute
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=08:00:00
+#SBATCH --job-name="get_features"
+#SBATCH --output=LOG/get_features.%j.out
+#SBATCH --error=LOG/get_features.%j.out
+#SBATCH --export=ALL
+
+source ~/.bashrc
+conda activate TRR181L4
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+python3 ./../pycompo/get_features.py "${CONFIG_FILE}"
+EOF
+
+# BUILD COMPOSITES
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=mh0731
+#SBATCH --partition=compute
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=08:00:00
+#SBATCH --job-name="get_composite"
+#SBATCH --output=LOG/get_composite.%j.out
+#SBATCH --error=LOG/get_composite.%j.out
+#SBATCH --export=ALL
+
+source ~/.bashrc
+conda activate TRR181L4
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+python3 ./../pycompo/get_composite.py "${CONFIG_FILE}"
+EOF
