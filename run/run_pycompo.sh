e5200c709784c8352d7477b72206ec5a7b972b10
Henning Franke <henning.franke@mpimet.mpg.de>
2025-09-03 14:17:41 +0200

    First operational processing of the full analysis dataset.

--- run/run_pycompo.sh
+++ run/run_pycompo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILE=/home/m/m300738/libs/pycompo/config/settings_template.yaml
-RUNFILE1=/home/m/m300738/libs/pycompo/pycompo/api/get_features.py
+CONFIG_FILE=/home/m/m300738/libs/pycompo/config/settings_ngc5004_pc02.yaml
+RUNFILE1=/home/m/m300738/libs/pycompo/pycompo/api/get_features_full.py
 
 # GET FEATURES
 sbatch <<EOF
@@ -10,7 +10,7 @@ sbatch <<EOF
 #SBATCH --partition=compute
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --job-name="get_features"
 #SBATCH --output=LOG/get_features.%j.out
 #SBATCH --error=LOG/get_features.%j.out
@@ -26,27 +26,3 @@ export NUMEXPR_NUM_THREADS=1
 
 python3 "${RUNFILE1}" "${CONFIG_FILE}"
 EOF
-
-## BUILD COMPOSITES
-#sbatch <<EOF
-##!/bin/bash
-##SBATCH --account=mh0731
-##SBATCH --partition=compute
-##SBATCH --ntasks-per-node=1
-##SBATCH --nodes=1
-##SBATCH --time=08:00:00
-##SBATCH --job-name="get_composite"
-##SBATCH --output=LOG/get_composite.%j.out
-##SBATCH --error=LOG/get_composite.%j.out
-##SBATCH --export=ALL
-#
-#source ~/.bashrc
-#conda activate TRR181L4
-#
-#export OMP_NUM_THREADS=1
-#export MKL_NUM_THREADS=1
-#export OPENBLAS_NUM_THREADS=1
-#export NUMEXPR_NUM_THREADS=1
-#
-#python3 ./../pycompo/get_composite.py "${CONFIG_FILE}"
-#EOF
