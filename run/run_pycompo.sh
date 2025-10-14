a7593600187f14bfcfa595f733efe5f5da7d161c
Henning Franke <henning.franke@mpimet.mpg.de>
2025-10-14 09:27:42 +0200

    Provide necessary inputs as command-line arguments. Thereby, the runscript itself doesn't need to be changed as soon as a new analysis should be run.

--- run/run_pycompo.sh
+++ run/run_pycompo.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-CONFIG_FILE=/home/m/m300738/libs/pycompo/config/settings_ngc5004_pc04.yaml
-RUNFILE1=/home/m/m300738/libs/pycompo/pycompo/api/get_features.py
-RUNFILE2=/home/m/m300738/libs/pycompo/pycompo/api/combine_feature_props.py
-RUNFILE3=/home/m/m300738/libs/pycompo/pycompo/api/get_composites.py
+analysis_identifier=${1}
+node_size=${2:-"256G"}
+if [ -z "$analysis_identifier" ]; then
+    echo "Usage: $0 <analysis_identifier>"
+    exit 1
+fi
+
+export CONFIG_FILE=/home/m/m300738/libs/pycompo/config/settings_${analysis_identifier}.yaml
+export RUNFILE1=/home/m/m300738/libs/pycompo/pycompo/api/get_features.py
+export RUNFILE2=/home/m/m300738/libs/pycompo/pycompo/api/combine_feature_props.py
+export RUNFILE3=/home/m/m300738/libs/pycompo/pycompo/api/get_composites.py
 
 # GET FEATURES
-jobid1 = $(sbatch --parsable <<EOF
+jobid1=$(sbatch --parsable --constraint=${node_size} <<EOF
 #!/bin/bash
 #SBATCH --account=mh0731
 #SBATCH --partition=compute
+#SBATCH --mem=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=08:00:00
@@ -29,13 +37,13 @@ export NUMEXPR_NUM_THREADS=1
 python3 "${RUNFILE1}" "${CONFIG_FILE}"
 EOF
 )
-
-
+	  
 # COMBINE FEATURE PROPS
-jobid2 = $(sbatch --parsable --dependency=afterok:${jobid1} <<EOF
+jobid2=$(sbatch --parsable --constraint=${node_size} --dependency=afterok:${jobid1} <<EOF
 #!/bin/bash
 #SBATCH --account=mh0731
 #SBATCH --partition=compute
+#SBATCH --mem=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=08:00:00
@@ -57,10 +65,11 @@ EOF
 )
 
 # GET COMPOSITES
-jobid3 = $(sbatch --parsable --dependency=afterok:${jobid2} sbatch <<EOF
+jobid3=$(sbatch --parsable --constraint=${node_size} --dependency=afterok:${jobid2} <<EOF
 #!/bin/bash
 #SBATCH --account=mh0731
 #SBATCH --partition=compute
+#SBATCH --mem=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=08:00:00
@@ -80,3 +89,5 @@ export NUMEXPR_NUM_THREADS=1
 python3 "${RUNFILE3}" "${CONFIG_FILE}"
 EOF
 )
+
+squeue -u $USER
